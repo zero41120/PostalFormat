347015f1294896a7c6a88c188435baeadbc79b3e
tlin34 <tlin34@ucsc.edu>
2016-12-15 23:18:52 +0800

    Format is able to read string array and ready to get to actual testing
    Implementation detail on shipping label requires the actual printer and
    the label.

--- src/postal/FormatProvider.java
+++ src/postal/FormatProvider.java
@@ -2,6 +2,8 @@ package postal;
 
 import java.util.ArrayList;
 
+import util.StringUtil;
+
 public class FormatProvider {
 
 	public static ArrayList<String> getFirstRow() {
@@ -16,6 +18,30 @@ public class FormatProvider {
 		out.add("收件人地址");
 		out.add("收件人電話");
 		out.add("收件人手機");
+		return out;
+	}
+	
+	public static String getPrintFormat(ArrayList<String> arr){
+		String out = arr.toString();
+		StringUtil util = new StringUtil();
+		for (int i = 0; i < arr.size(); i++) {
+			String temp = "";
+			switch (i) {
+			case 0: temp += util.padFor(arr.get(i), "\n", 3); break;
+			case 1: temp += util.padFor(arr.get(i), "|", 3); break;
+			case 2: temp += util.padFor(arr.get(i), "\n", 3); break;
+			case 3: temp += util.padFor(arr.get(i), "\t", 3); break;
+			case 4: temp += util.padFor(arr.get(i), ".", 3); break;
+			case 5: temp += util.padFor(arr.get(i), "~", 3); break;
+			case 6: temp += util.padFor(arr.get(i), "$", 3); break;
+			case 7: temp += util.padFor(arr.get(i), "^", 3); break;
+			case 8: temp += util.padFor(arr.get(i), "&", 3); break;
+			case 9: temp += util.padFor(arr.get(i), "*", 3); break;
+			case 10: temp += util.padFor(arr.get(i), "(", 3); break;
+			default: break;
+			}
+			out += temp;
+		}
 		return out;
 	}
 
