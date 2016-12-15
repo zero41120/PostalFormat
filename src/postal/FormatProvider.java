6872d6b3a20af4e59c96d0bca98da9f43be257d7
tlin34 <tlin34@ucsc.edu>
2016-12-15 20:56:01 +0800

    Basic format for shipping label
    Basic format for shipping label. It will probably be a good idea to
    make address into smaller sections, such as city and hours number.
    Implementation details will be discussed with the client.

--- src/postal/FormatProvider.java
+++ src/postal/FormatProvider.java
@@ -1,5 +1,22 @@
 package postal;
 
+import java.util.ArrayList;
+
 public class FormatProvider {
+
+	public static ArrayList<String> getFirstRow() {
+		ArrayList<String> out = new ArrayList<>();
+		out.add("寄件人姓名");
+		out.add("寄件人郵遞區號");
+		out.add("寄件人地址");
+		out.add("寄件人電話");
+		out.add("寄件人手機");
+		out.add("收件人名字");
+		out.add("收件人郵遞區號");
+		out.add("收件人地址");
+		out.add("收件人電話");
+		out.add("收件人手機");
+		return out;
+	}
 
 }
