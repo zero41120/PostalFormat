149d7f9953b97c32c5aa4fe9cc69d8353d785a4a
tlin34 <tlin34@ucsc.edu>
2016-12-15 23:15:43 +0800

    Add get cell data as array list.
    Apache’s official document has CellType as the value to set cell type.
    I attempted to set the cell type to string before extraction. However
    the jar file I had is probably outdated, I couldn’t get it to work. I
    use try catch to handle none string cells.

--- src/util/ExcelManager.java
+++ src/util/ExcelManager.java
@@ -1,9 +1,12 @@
 package util;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public abstract class ExcelManager extends Manager {
 
@@ -13,13 +16,33 @@ public abstract class ExcelManager extends Manager {
 			cell.setCellValue(arr.get(i));
 		}
 	}
+	
+	public ArrayList<String> getCellAsArrayString(XSSFRow row, Integer begin, Integer end){
+		ArrayList<String> data = new ArrayList<>();
+		for (int i = begin; i < end; i++) {
+			XSSFCell cell = row.getCell(i);
+			try {
+				data.add(cell.getStringCellValue());
+			} catch (Exception e){
+				data.add(cell.getRawValue().toString());
+			}
+		}
+		ArrayUtil<String> aUtil = new ArrayUtil<>();
+		
+		return aUtil.isAllEqual(data)? (new ArrayList<String>()) : data;
+	}
 
 	public boolean isEqualCellArrayString(XSSFRow row, ArrayList<String> arr) {
 		try {
 			for (int i = 0; i < arr.size(); i++) {
 				XSSFCell cell = row.getCell(i);
-				if (!cell.getStringCellValue().equals(arr.get(i)))
+				try {
+					if (!cell.getStringCellValue().equals(arr.get(i)))
+						return false;
+				} catch (Exception e){
 					return false;
+				}
+					
 			}
 		} catch (Exception e) {
 			// Probably array out of bound. 
@@ -27,5 +50,11 @@ public abstract class ExcelManager extends Manager {
 			return false;
 		}
 		return true;
+	}
+	
+	public XSSFSheet getSheet(File workingFile, Integer index) throws Exception {
+		XSSFWorkbook wb = new XSSFWorkbook(workingFile);
+		wb.close();
+		return wb.getSheetAt(index);		
 	}
 }
